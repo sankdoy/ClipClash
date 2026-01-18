13abcb7bfa6ad81a77469a5de636eb023b98d832
sankdoy <edjarv03@gmail.com>
2026-01-18 00:02:25 +0000

    Chunk 5: enable unused checks and remove sponsor page endpoint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,5 +4,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
   settings: { react: { version: 'detect' } },
-  rules: {}
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+  }
 }
